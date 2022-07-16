54a36ec492e9d7c60fff4d51fdd1ca38f73813bf
Peter Vlk <petervlk86@gmail.com>
2022-07-17 00:46:11 +0200

    update faces config

--- early-config.el
+++ early-config.el
@@ -5,19 +5,20 @@
 ;; Author:  <vlko@archon>
 ;; Keywords: elisp
 
-;; Set further font and theme customizations
-(custom-set-variables
-   '(rational-ui-default-font
-     '(:font "JetBrains Mono" :weight light :height 110)))
-
-
 ;; Fullscreen by default, as early as possible. This tiny window is not enough
 (set-frame-parameter (selected-frame) 'fullscreen 'maximized)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
+;;; Font Faces
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (custom-set-faces
+             `(default ((t (:font "JetBrains Mono Light 11"))))
+             `(fixed-pitch ((t (:inherit (default)))))
+             `(fixed-pitch-serif ((t (:inherit (default)))))
+             `(variable-pitch ((t (:font "Arial 18")))))))
 
-;; Setup modus themes
-;; Add all your customizations prior to loading the themes
+;;; Theme
 (setq modus-themes-italic-constructs t
       modus-themes-bold-constructs nil
       modus-themes-region '(bg-only))
